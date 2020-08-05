ba8c8e138fdd562ffa96ee88be01de7f4268ddc0
gnanaguru <techadmin@virujh.com>
2020-08-05 14:39:51 +0530

    new changes

---
+++ build/precache-manifest.374ce9b55c5a5201e4bd8d8653044329.js
@@ -0,0 +1,118 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "13a3eb306b954531a0d072353ac6933d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3795d8d7614a33c3f4be",
+    "url": "/static/css/main.cc34cd24.chunk.css"
+  },
+  {
+    "revision": "4f638c95559a9863450e",
+    "url": "/static/js/2.725c0d0b.chunk.js"
+  },
+  {
+    "revision": "c232dc3342581f78f58f977c25531a9e",
+    "url": "/static/js/2.725c0d0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3795d8d7614a33c3f4be",
+    "url": "/static/js/main.86eaf6ba.chunk.js"
+  },
+  {
+    "revision": "37834f175874a248dc3e",
+    "url": "/static/js/runtime-main.ff96d2cb.js"
+  },
+  {
+    "revision": "5c1c6efb08e53839587f705cd35eb9d3",
+    "url": "/static/media/No-Appoinments.5c1c6efb.svg"
+  },
+  {
+    "revision": "1358b04db31232c95cea0ac9ea830264",
+    "url": "/static/media/ProductSans-BoldItalic.1358b04d.svg"
+  },
+  {
+    "revision": "70caf0c8df1379502dea9b0b3e52f68e",
+    "url": "/static/media/ProductSans-BoldItalic.70caf0c8.ttf"
+  },
+  {
+    "revision": "8042801145ae6c23d914c56dcebdd5f8",
+    "url": "/static/media/ProductSans-BoldItalic.80428011.eot"
+  },
+  {
+    "revision": "c8293b1aa94dbf7d293d578542a5855c",
+    "url": "/static/media/ProductSans-BoldItalic.c8293b1a.woff"
+  },
+  {
+    "revision": "27b6dc324137dc58114f9b5ad1cb51a7",
+    "url": "/static/media/ProductSans-Medium.27b6dc32.ttf"
+  },
+  {
+    "revision": "5df58501ff2b7bf64aec872337e436df",
+    "url": "/static/media/ProductSans-Regular.5df58501.svg"
+  },
+  {
+    "revision": "aee051e43f3a6751a893cdbbe14365d1",
+    "url": "/static/media/ProductSans-Regular.aee051e4.eot"
+  },
+  {
+    "revision": "caa39dce486ed74ea2bd287f161e2baf",
+    "url": "/static/media/ProductSans-Regular.caa39dce.ttf"
+  },
+  {
+    "revision": "d3e70bb9f3e0c92a66905d70ba60d740",
+    "url": "/static/media/ProductSans-Regular.d3e70bb9.woff"
+  },
+  {
+    "revision": "09ab14ac355c562a2811957a871a3977",
+    "url": "/static/media/appointments.09ab14ac.svg"
+  },
+  {
+    "revision": "0c42c6221a57d700aa59e7528f464ea6",
+    "url": "/static/media/bg.0c42c622.png"
+  },
+  {
+    "revision": "aa93464062894e6f9bef344382baf45f",
+    "url": "/static/media/dot-menu.aa934640.svg"
+  },
+  {
+    "revision": "cf1caa570f09c213d1953ffeb20a072a",
+    "url": "/static/media/filter.cf1caa57.svg"
+  },
+  {
+    "revision": "78be083b3b829dcc8ca2a441fb502223",
+    "url": "/static/media/hospitalLogo.78be083b.png"
+  },
+  {
+    "revision": "4ff13c57e059d7b6f72bf179a7d9bbfc",
+    "url": "/static/media/icomoon.4ff13c57.svg"
+  },
+  {
+    "revision": "b85ba9c58c1b7a4f6b7978d8be15e2b0",
+    "url": "/static/media/icomoon.b85ba9c5.eot"
+  },
+  {
+    "revision": "d6e1cb1df7f93dc7fe619819c5696994",
+    "url": "/static/media/icomoon.d6e1cb1d.woff"
+  },
+  {
+    "revision": "dda5bdac6a87999896a2e9b86de3fa9a",
+    "url": "/static/media/icomoon.dda5bdac.ttf"
+  },
+  {
+    "revision": "00a68b73a2cf35c64f017e78aee7c6dd",
+    "url": "/static/media/logo-white.00a68b73.svg"
+  },
+  {
+    "revision": "0a9eb673028387488b380985cd678357",
+    "url": "/static/media/logo.0a9eb673.png"
+  },
+  {
+    "revision": "bedc5594fb268ebe5f950dc77516ac9b",
+    "url": "/static/media/right.bedc5594.svg"
+  },
+  {
+    "revision": "b802980dae17fe0784eb0a4fdf0c1564",
+    "url": "/static/media/view.b802980d.svg"
+  }
+]);
