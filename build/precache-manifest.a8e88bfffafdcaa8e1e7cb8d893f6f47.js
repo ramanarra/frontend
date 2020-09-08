2563f4f0c338e223cb0de6b128910a561ae91e94
gnanaguru <techadmin@virujh.com>
2020-09-08 19:35:59 +0530

    new changes in doctor pages

---
+++ build/precache-manifest.a8e88bfffafdcaa8e1e7cb8d893f6f47.js
@@ -0,0 +1,178 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e105790d416ee8013805ece1383f36b",
+    "url": "/app/index.html"
+  },
+  {
+    "revision": "e784bca723645b704d10",
+    "url": "/app/static/css/main.eeddbd59.chunk.css"
+  },
+  {
+    "revision": "fb9fb9bda8db74ba0c5f",
+    "url": "/app/static/js/2.db71574e.chunk.js"
+  },
+  {
+    "revision": "c98a37c56683340baad2bdf638edf5a2",
+    "url": "/app/static/js/2.db71574e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e784bca723645b704d10",
+    "url": "/app/static/js/main.ade1faac.chunk.js"
+  },
+  {
+    "revision": "c02c8c74c9c072070c5c",
+    "url": "/app/static/js/runtime-main.c9b5768c.js"
+  },
+  {
+    "revision": "5c1c6efb08e53839587f705cd35eb9d3",
+    "url": "/app/static/media/No-Appoinments.5c1c6efb.svg"
+  },
+  {
+    "revision": "1358b04db31232c95cea0ac9ea830264",
+    "url": "/app/static/media/ProductSans-BoldItalic.1358b04d.svg"
+  },
+  {
+    "revision": "70caf0c8df1379502dea9b0b3e52f68e",
+    "url": "/app/static/media/ProductSans-BoldItalic.70caf0c8.ttf"
+  },
+  {
+    "revision": "8042801145ae6c23d914c56dcebdd5f8",
+    "url": "/app/static/media/ProductSans-BoldItalic.80428011.eot"
+  },
+  {
+    "revision": "c8293b1aa94dbf7d293d578542a5855c",
+    "url": "/app/static/media/ProductSans-BoldItalic.c8293b1a.woff"
+  },
+  {
+    "revision": "27b6dc324137dc58114f9b5ad1cb51a7",
+    "url": "/app/static/media/ProductSans-Medium.27b6dc32.ttf"
+  },
+  {
+    "revision": "5df58501ff2b7bf64aec872337e436df",
+    "url": "/app/static/media/ProductSans-Regular.5df58501.svg"
+  },
+  {
+    "revision": "aee051e43f3a6751a893cdbbe14365d1",
+    "url": "/app/static/media/ProductSans-Regular.aee051e4.eot"
+  },
+  {
+    "revision": "caa39dce486ed74ea2bd287f161e2baf",
+    "url": "/app/static/media/ProductSans-Regular.caa39dce.ttf"
+  },
+  {
+    "revision": "d3e70bb9f3e0c92a66905d70ba60d740",
+    "url": "/app/static/media/ProductSans-Regular.d3e70bb9.woff"
+  },
+  {
+    "revision": "a2630e5bb68746c2ef42080d0fcc588c",
+    "url": "/app/static/media/Schedule.a2630e5b.svg"
+  },
+  {
+    "revision": "253a9959071858e1fffa3efa37f89740",
+    "url": "/app/static/media/appointmentIcon.253a9959.svg"
+  },
+  {
+    "revision": "09ab14ac355c562a2811957a871a3977",
+    "url": "/app/static/media/appointments.09ab14ac.svg"
+  },
+  {
+    "revision": "d5d8f9b04552a118bafb078199db253a",
+    "url": "/app/static/media/calender2.d5d8f9b0.svg"
+  },
+  {
+    "revision": "35c33b7fc8b10387424947329709526d",
+    "url": "/app/static/media/call-on.35c33b7f.svg"
+  },
+  {
+    "revision": "e83b5deaa00bed02a98d16655dedef95",
+    "url": "/app/static/media/camera-off.e83b5dea.svg"
+  },
+  {
+    "revision": "2a588b6884359cdef192b5cc754e3970",
+    "url": "/app/static/media/chat-icon.2a588b68.svg"
+  },
+  {
+    "revision": "aa93464062894e6f9bef344382baf45f",
+    "url": "/app/static/media/dot-menu.aa934640.svg"
+  },
+  {
+    "revision": "e2bc5679ae413d9f6734fcf20934e5b6",
+    "url": "/app/static/media/eye.e2bc5679.svg"
+  },
+  {
+    "revision": "cf1caa570f09c213d1953ffeb20a072a",
+    "url": "/app/static/media/filter.cf1caa57.svg"
+  },
+  {
+    "revision": "78be083b3b829dcc8ca2a441fb502223",
+    "url": "/app/static/media/hospitalLogo.78be083b.png"
+  },
+  {
+    "revision": "4ff13c57e059d7b6f72bf179a7d9bbfc",
+    "url": "/app/static/media/icomoon.4ff13c57.svg"
+  },
+  {
+    "revision": "b85ba9c58c1b7a4f6b7978d8be15e2b0",
+    "url": "/app/static/media/icomoon.b85ba9c5.eot"
+  },
+  {
+    "revision": "d6e1cb1df7f93dc7fe619819c5696994",
+    "url": "/app/static/media/icomoon.d6e1cb1d.woff"
+  },
+  {
+    "revision": "dda5bdac6a87999896a2e9b86de3fa9a",
+    "url": "/app/static/media/icomoon.dda5bdac.ttf"
+  },
+  {
+    "revision": "183ca272bafccb438f2bf8d329a5eeec",
+    "url": "/app/static/media/join.183ca272.svg"
+  },
+  {
+    "revision": "1444aa68aa1e3e7db1ca4d6bb49211db",
+    "url": "/app/static/media/left-arrow.1444aa68.svg"
+  },
+  {
+    "revision": "0a9eb673028387488b380985cd678357",
+    "url": "/app/static/media/logo.0a9eb673.png"
+  },
+  {
+    "revision": "d67c4b03212ccceba4b25e570c8b5828",
+    "url": "/app/static/media/mic-off.d67c4b03.svg"
+  },
+  {
+    "revision": "7ae5b403b6a6c993ef5a8f79c1441c73",
+    "url": "/app/static/media/mic-on.7ae5b403.svg"
+  },
+  {
+    "revision": "a084e2b4e8193338c7daecbab106d4ac",
+    "url": "/app/static/media/past-appointments.a084e2b4.svg"
+  },
+  {
+    "revision": "a78346bd3f0124409ccc832c0bacbd35",
+    "url": "/app/static/media/sent-icon.a78346bd.svg"
+  },
+  {
+    "revision": "a86600e909ee32ceca29d6a8cf81e22e",
+    "url": "/app/static/media/tab.a86600e9.svg"
+  },
+  {
+    "revision": "21578b7900b17225931aeea93b14ffc7",
+    "url": "/app/static/media/video-off.21578b79.svg"
+  },
+  {
+    "revision": "4526c06524a5f2c36a354cc12eb6be5b",
+    "url": "/app/static/media/video-on.4526c065.svg"
+  },
+  {
+    "revision": "b802980dae17fe0784eb0a4fdf0c1564",
+    "url": "/app/static/media/view.b802980d.svg"
+  },
+  {
+    "revision": "1eeebf0b3a0c58f5f55768aa52cb7bf3",
+    "url": "/app/static/media/wait.1eeebf0b.svg"
+  },
+  {
+    "revision": "e23cbea877e477db39b33b2ff758e213",
+    "url": "/app/static/media/waiting.e23cbea8.svg"
+  }
+]);
