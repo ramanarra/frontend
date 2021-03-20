7202f88ee1ec6d90db30cdb20a768aa5562294bd
Indhu <indhumathi@softsuave.com>
2021-03-20 19:10:59 +0530

    build files update

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.f17c587bac0fcfdb057064ac5de0bca6.js"
+  "/app/precache-manifest.9ff1f3024f22f35ef14ac18c93c43ffa.js"
 );
 
 self.addEventListener('message', (event) => {
