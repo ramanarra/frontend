b0d10c526464bf5a7abd310c005a294586a7ba9a
manojsoftsuave <manoj.rajendran@softsuave.com>
2021-03-26 20:22:42 +0530

    dev-build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app/precache-manifest.265e1f6bc4361656f2be49704dba54de.js"
+  "/app/precache-manifest.5b4abea5f2886ca391a4a4c3ceaf86c3.js"
 );
 
 self.addEventListener('message', (event) => {
