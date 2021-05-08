0df061a119a35b44f9a0da403d355be0afc4409c
Ponsundaram <ponsundaram@softsuave.com>
2021-05-08 17:08:47 +0530

    [Update][Consultation timing diff]Triggered automated timing on detail page

--- src/pages/AppointmentDetails/index.jsx
+++ src/pages/AppointmentDetails/index.jsx
@@ -266,16 +266,17 @@ function AppoinmentDetails() {
     moment(currentTime, 'DD/MM/YYYY HH:mm:ss A')
   )
 
-  
   let differenceInDays = moment.duration(difference)
+
+  const [seconds, setSeconds] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
+      setSeconds(seconds => seconds + 1);
       differenceInDays = moment.duration(difference)
     }, 1000);
     return () => clearInterval(interval);
   }, []);
-
 
   const days =
     NumberToWords.toWords(differenceInDays.days()).charAt(0).toUpperCase() +
@@ -611,6 +612,7 @@ function AppoinmentDetails() {
                     >{`${minutes} more minutes to join`}</Typography>
                   ))
                 )}
+                <Typography style={{display: 'none'}}>{seconds}</Typography>
               </Box>
             </Box>
 
